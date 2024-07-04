42570fdc564cc0fcee84743fa839fc017520bdcc
Bensoli <benswambua96@gmail.com>
2024-07-04 16:19:29 +0300

    range function

---
+++ rangeFunction.py
@@ -0,0 +1,13 @@
+#inbuilt function to generate a sequence of numbers
+
+numbers = range(5, 10)
+print(numbers)
+
+#to see the actual numbers we need to iterate over the range using a for loop
+for i in numbers:
+    print(i)
+
+#jumping two 2 steps at a time
+numberss = range(5, 10, 2)
+for k in numberss:
+    print(k)
