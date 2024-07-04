fb58fb832d4b36a1f7082665997cb7455c374ce6
Bensoli <benswambua96@gmail.com>
2024-07-04 15:33:29 +0300

    if statements

---
+++ ifStatements.py
@@ -0,0 +1,14 @@
+#used to make decisions
+temperature =28
+
+if temperature>30:
+    print("hot day")
+    print("drink a lot of water")
+elif temperature>20:
+    print("its a nice day")
+elif temperature>10:
+    print("It's a bit cold")
+else:
+    print("its cold")
+
+print("Done")
