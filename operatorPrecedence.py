2407f1049493c0eaf45d0706c02218496dadae64
Bensoli <benswambua96@gmail.com>
2024-07-04 14:36:21 +0300

    operator precedence

---
+++ operatorPrecedence.py
@@ -0,0 +1,7 @@
+x = 10+3*2
+print(x) #multiplication gets evaluated first then addition
+
+
+#you can change the precedence by adding brackets
+y = (10+3)*2
+print(y) #multiplication gets evaluated first then addition
