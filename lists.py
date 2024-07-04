e5965e58aa379973ee1f91e396622b508a3dbbf8
Bensoli <benswambua96@gmail.com>
2024-07-04 15:54:35 +0300

    lists

---
+++ lists.py
@@ -0,0 +1,13 @@
+#used to represent a list of objects
+names = ["musili", "bensoli", "bastion", "emogy"]
+print(names)
+
+#to get 1st element in the list
+print(names[0])
+
+#using negative index
+print(names[-1])  #represents last element
+
+#printing several indexes
+print(names[0:3]) #python will exclude the lats index
+
