16de41a2fd13d416f7f73173d19ec873546355e3
Bensoli <benswambua96@gmail.com>
2024-07-04 14:30:53 +0300

    arithmetic operations

---
+++ Arithmetic.py
@@ -0,0 +1,22 @@
+print(10+3) #addition operator
+print(10-3) #subtraction operator
+print(10*3) #multiplication operator
+print(10/3) #division operator / gives a floating number
+
+#division with 2 slashes // gives an integer/whole number
+print(10//3)
+
+#modules operator % it gives the remainder of the division result
+print(10%3)
+
+#exponent operator ** represents to the power of
+print(10 **3)
+
+#argumenteted assignnmnet operator +=
+x=10
+x = x+3 #incrementing value of x by 3
+print(x)
+#now using less code
+y=20
+y+= 3
+print(y)
